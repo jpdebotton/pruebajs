fb974389a2b8e4f3cc509cd619fbac017e1fe4aa
Juan Pablo de Botton <jpdebotton@gmail.com>
2016-10-12 21:57:48 -0500

    Pregunta 7

---
+++ Pregunta4/main.js
@@ -0,0 +1,8 @@
+// Pregunta 4
+
+// var n = prompt("Ingresa el lado del cuadradado");
+
+$(document).ready(function(){
+	var espacios = "    ";
+	$('.p1').text("* "+" \ "+" *");
+	});
---
+++ Pregunta7/main.js
@@ -0,0 +1,6 @@
+// Pregunta 7
+$(document).ready(function(){
+$('#evento').on('click',function(){
+$("#evento > p").css("background-color","blue");
+	});
+});
